d65d0f8a621b75db554e4d5ead9e288ac3a6fc4c
Vaidotak <pehtrauhk@gmail.com>
2022-05-31 15:38:47 +0300

    pridėti komentarai

--- script.js
+++ script.js
@@ -1,52 +1,3 @@
-// console.log('veikia')
-
-// // konstruktorius
-
-// function Cars(marke, spalva, duruSkaicius) {
-//     this.marke = marke
-//     this.spalva = spalva
-//     this.duruSkaicius = duruSkaicius
-//     this.raktas = function(raktasM){
-//       let secretKey = raktasM
-//       console.log(secretKey)
-//     }
-
-//   }
-
-//   const myCar = new Cars('Toyota', 'juoda', 5)
-//   const myCar1 = new Cars('Audi', 'balta', 3)
-//   const myCar3 = new Cars('BMW', 'Žalia', 5)
-
-//   myCar.raktas(85858 + `e6590487i`)
-//   myCar1.raktas(9 + `e6590487i`)
-//   myCar3.raktas(3843374 + `e6590487i`)
-
-//   // klasė
-
-//   const ages = [33, 32, 16];
-//   console.log(ages.filter((age) => age > 20))
-//   // naujas masyvas iš norimų reikšmių
-
-//   const numbers = [1, 2, 3, 4];
-
-//   numbers.forEach((num) => {
-//     const square = num * num;
-//     console.log("Skaičiaus kvadratas: " + square);
-//   });
-
-// function sliceFruit(fruit) {
-//   console.log("Pjaustau " + fruit + "!")
-// }
-
-// const fruits = ["🍎", "🍊", "🍋", "🍓", "🥝"]
-
-// fruits.forEach(sliceFruit)
-// const food = ["🍔", "🍟", "🍦"]
-
-// food.forEach((item) => {
-//   console.log("Nupirk " + item + "!")
-// })
-
 // https://httpdump.io/
 
 let endpoint = "https://httpdump.io/oour3";
@@ -93,7 +44,7 @@ function createInput() {
   inputContainer.append(myInput, xButton);
   ciaSugulsInput.appendChild(inputContainer);
 
-  xButton.addEventListener("click", function () {
+  xButton.addEventListener("click", function () { //trina myInput & xButton
     this.parentElement.remove();
   });
 }
